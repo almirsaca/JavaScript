3878414ce22a5d7d274194d8ef9a6e4a5007163e
almir.martinelli <almir.martinelli@gmail.com>
2021-12-07 10:17:01 -0300

    Finalizada Aula 2 - var, let e const

--- Fundamentos do JavaScript/Aula 1 - Tipos primitivos/Tipo-booleano/typer-booleano.js
+++ Fundamentos do JavaScript/Aula 1 - Tipos primitivos/Tipo-booleano/typer-booleano.js
@@ -1,7 +1,62 @@
-const pNum = 5;
-const sNum = 18;
-let cadastroAtivado = true;
+{
+    const pNum = 5;
+    const sNum = 18;
+    let cadastroAtivado = true;
 
-console.clear();
+    console.clear();
 
-console.log(pNum === sNum);
+    console.log("============== I N I C I O =====================");
+
+    noMeioCod = "ahhhhhhhhhhhhhhhhhhh";
+
+    console.log(noMeioCod);
+
+    console.log(pNum === sNum);
+
+    const minhaVar = 1;
+    const MinhaVar = "texto";
+    const minhavar = "3";
+    const MINHAVAR = 2;
+
+    console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+    let input = null;
+    let input2;
+
+    if (input === null) {
+    console.log('não há informação');
+    } else {
+    console.log(input);
+    }
+
+    var noMeioCod;
+
+    input = undefined;
+    console.log(input);
+    console.log(input2); // undefined
+
+    var t = null == undefined;
+    console.log("null == undefined => " + t);
+    console.log(null == undefined); // true
+    console.log("null === undefined");
+    console.log(null === undefined); // false
+}
+
+var noMeioCod = 10;
+
+console.log(noMeioCod);
+
+console.log("============== exibeMensagem =====================");
+
+var exibeMensagem = function() { 
+    console.log(mensagemDentroDoIf); // Alura 
+    var mensagemForaDoIf = 'Caelum'; 
+    if(true) { 
+        var mensagemDentroDoIf = 'Alura'; 
+        console.log(mensagemDentroDoIf)// Alura ;
+    } 
+    console.log(mensagemForaDoIf); // Caelum 
+
+    console.log(mensagemDentroDoIf); // Alura 
+}
+
