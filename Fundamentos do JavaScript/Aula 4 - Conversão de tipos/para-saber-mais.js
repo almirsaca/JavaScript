12526eafc85fb2d34fa62950a683bff2b307968f
almir.martinelli <almir.martinelli@gmail.com>
2021-12-09 09:55:51 -0300

    Finalizado - 06 Para saber mais: Palavras reservadas

---
+++ Fundamentos do JavaScript/Aula 4 - Conversão de tipos/para-saber-mais.js
@@ -0,0 +1,7 @@
+
+let telefone = 12341234;
+// teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+console.log("O telefone é " + telefone); 
+console.log("O telefone é " + String(telefone));
+ // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+console.log('O telefone é ' + telefone.toString());
