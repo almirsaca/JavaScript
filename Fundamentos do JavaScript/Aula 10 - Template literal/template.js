312ae8996a8e6357e73ef065fde2bf0dbcc2d72c
almir.martinelli <almir.martinelli@gmail.com>
2021-12-15 10:07:42 -0300

    Finalizada a aula Template literal

---
+++ Fundamentos do JavaScript/Aula 10 - Template literal/template.js
@@ -0,0 +1,9 @@
+const nome = 'Almir';
+const idade = 2022 - 1970;
+const cidade = 'São Paulo';
+
+//console.log('Aluno ' + nome + ' idade ' + idade + ' nacimento ' + cidade);
+
+const apresentacao = "Aluno ${nome}, idade ${idade} e nacimento na cidade de ${cidade}";
+
+console.log(apresentacao);
